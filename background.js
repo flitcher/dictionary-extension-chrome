78b7da9f99fddd0994d676875f6178da764181e5
quintin <mmarty98@hotmail.com>
2018-05-12 16:30:31 -0700

    initial setup and able to send selected text to content script

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
-console.log("background script running");
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-c
+
+console.log('background running!!!');
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+	console.log(tab);
+}
--- content.js
+++ content.js
@@ -1,1 +1,8 @@
-console.log("hello");
+console.log('content script!!!!!')
+
+window.addEventListener('mouseup', wordSelected);
+
+function wordSelected() {
+	let selectedText = window.getSelection().toString();
+	console.log(selectedText);
+}
