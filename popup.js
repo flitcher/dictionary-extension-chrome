d97acb6fa01ad99fe30db811e92794888f239189
quintin <mmarty98@hotmail.com>
2018-05-25 23:43:16 -0700

    api call successful

--- popup.js
+++ popup.js
@@ -1,7 +1,27 @@
 let bgpage = chrome.extension.getBackgroundPage();
 let word = bgpage.word;
 
-let url = `https://api.wordnik.com/v4/word.json/apple/definitions?limit=1&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOURAPIKEY`;
+let url = `https://api.wordnik.com/v4/word.json/
+${word}
+/definitions?limit=200
+&includeRelated=false
+&sourceDictionaries=all
+&useCanonical=false
+&includeTags=false
+&api_key=91f1dfbb415256f52f2013b27935ee7e8ffa5ac02f8f7dd85`;
 
-document.getElementById("userselect").innerHTML = word;
+url = url.replace(/\s+/g, '');
+
+$(function () {
+
+	$.ajax({
+		type: 'GET',
+		url: url,
+		success: function(data) {
+			document.getElementById("userselect").innerHTML = data[0].text;
+		}
+	})
+})
+
+// document.getElementById("userselect").innerHTML = word;
 
